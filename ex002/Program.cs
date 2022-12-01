055e2efe949b9a1ceee715d8055d188f6e9d5606
siferus <35690525+siferus@users.noreply.github.com>
2022-12-01 17:10:40 +0300

    задача 2

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+if (B > max)
+{
+    max = B;
+}
+
+if (C > max)
+{
+    max = C;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
