4b149afcb6938e2a45c90f7808800e7fa9789c2a
siferus <35690525+siferus@users.noreply.github.com>
2022-12-01 17:10:52 +0300

    задача 3

--- ex003/Program.cs
+++ ex003/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 1)
+            {
+                Console.WriteLine("Число " + number + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + number + "является: ЧЁТНЫМ");
+            }
+            
