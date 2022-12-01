361444d1c64f5d523f4b4f402594fe6fab93d533
siferus <35690525+siferus@users.noreply.github.com>
2022-12-01 17:11:04 +0300

    задача 4

--- ex004/Program.cs
+++ ex004/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i = 1;
+            bool not = true;
+
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чётные числа от 1 до " + number);
+            while (i <= number)
+            {
+                if (i % 2 != 1)
+                {
+                    Console.Write(i + ", ");
+                    not = false;
+                }
+                i++;
+            }
+
+            if (not)
+            {
+                Console.WriteLine("Нет чётных чисел!");
+            }
+
