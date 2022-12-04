cf92d46d46ed60f60159268170fe1bdb8e8304fe
siferus <35690525+siferus@users.noreply.github.com>
2022-12-04 20:25:55 +0300

    задача 5

---
+++ ex005/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    num = num / 10 % 10;
+Console.WriteLine($"Вторая цифра введённого числа: {num}");
