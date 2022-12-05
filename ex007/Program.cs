0a855b2911b0f941a8a15d40fcd869c6a1300802
siferus <35690525+siferus@users.noreply.github.com>
2022-12-05 10:49:55 +0300

    задача 7

---
+++ ex007/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Apps 
+{ 
+    internal class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Console.Write(" Нужно узнать, является ли этот день выходным или нет?  "); 
+         
+                Console.Write("\n Введите номер дня недели  "); 
+                int num = int.Parse(Console.ReadLine()); 
+                if (num >= 1 && num <= 5) 
+                { 
+                    Console.Write(" Нет, это не выходной"); 
+                } 
+ 
+                else if (num >= 6 && num <= 7) 
+                { 
+                    Console.Write(" Да, это выходной"); 
+                } 
+                else Console.Write(" Не существует такого дня недели"); 
+            
+        } 
+    } 
+}
+
